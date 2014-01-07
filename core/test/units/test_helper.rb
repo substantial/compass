84318d499ecdc68aa27e892ffa13586a8bec2bdb
Scott Davis <me@sdavis.info>
2014-01-07 03:23:31 -0500

    move test helpers WIP

--- core/test/units/test_helper.rb
+++ core/test/units/test_helper.rb
@@ -4,5 +4,5 @@ require 'compass/core'
 require "test/unit"
 require File.expand_path(File.join(File.dirname(__FILE__), "..", "helpers", "diff"))
 
-include Compass::Diff
+include Compass::Test::Diff
 
